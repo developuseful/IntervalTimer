881bd717a97c328b14653f9f746551db2b62ec9b
Vyacheslav Pogudin <developuseful@gmail.com>
2026-04-03 17:14:11 +0800

    fix: Added imports to TimerRepository.kt and internet permissions

--- app/src/main/java/vyacheslav/pogudin/intervaltimer/data/repository/TimerRepository.kt
+++ app/src/main/java/vyacheslav/pogudin/intervaltimer/data/repository/TimerRepository.kt
@@ -1,6 +1,8 @@
 package vyacheslav.pogudin.intervaltimer.data.repository
 
 import vyacheslav.pogudin.intervaltimer.data.api.ApiService
+import vyacheslav.pogudin.intervaltimer.domain.mapper.toDomain
+import vyacheslav.pogudin.intervaltimer.domain.model.Timer
 
 class TimerRepository(private val api: ApiService) {
     suspend fun getTimer(id: Int): Timer {
